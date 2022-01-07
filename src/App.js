8b881d876c3b1d0b96c3eec1ae3e12b8a3cf27eb
dha-stix <asaoludavid234@yahoo.com>
2022-01-07 04:54:12 +0100

    Edit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import HomePage from './components/HomePage';
+import LandingPage from './components/LandingPage';
 import {Route, Routes} from "react-router-dom"
 import Dashboard from './components/Dashboard';
 import Question from './components/Question';
+
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
+        <Route path="/" element={<LandingPage/>}/>
         <Route path="dashboard" element={<Dashboard/>}/>
         <Route path="question/:course" element={<Question/>}/>
-        <Route path="*" element={<HomePage/>}/>
+        <Route path="*" element={<LandingPage/>}/>
       </Routes>
     </div>
   );
